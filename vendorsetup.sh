684873daadfa4e496c39b52e00d73ee0c7550619
Mayur <ultramayur123@gmail.com>
2022-05-16 10:47:19 +0530

    Revert "RMX2170: clone trb clang instead of proton"
    This reverts commit a6cc3ee4054ad3884ab972da54992bd6fd6ffaa1.
    
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,15 +8,15 @@ else
 fi
 
 # proton clang
-if [ -e prebuilts/clang/host/linux-x86/clang-trb ]; then
-    echo "TheRagingBeast clang exists... Skipping cloning." && echo "" && sleep 0.5
+if [ -e prebuilts/clang/host/linux-x86/clang-proton ]; then
+    echo "Proton clang exists... Skipping cloning." && echo "" && sleep 0.5
 else
-    echo "Now will clone TheRagingBeast clang" && echo ""
-    git clone --depth=1 https://gitlab.com/varunhardgamer/trb_clang prebuilts/clang/host/linux-x86/clang-trb
-    if [ -e prebuilts/clang/host/linux-x86/clang-trb ]; then
-        echo "Done cloning of TheRagingBeast clang" && echo "" && sleep 0.5
+    echo "Now will clone Proton clang" && echo ""
+    git clone --depth=1 https://github.com/kdrag0n/proton-clang prebuilts/clang/host/linux-x86/clang-proton
+    if [ -e prebuilts/clang/host/linux-x86/clang-proton ]; then
+        echo "Done cloning of Proton clang" && echo "" && sleep 0.5
     else
-        echo "There is some problem in cloning TheRagingBeast clang" && echo "" && sleep 0.5
+        echo "There is some problem in cloning Proton clang" && echo "" && sleep 0.5
     fi
 fi
 
