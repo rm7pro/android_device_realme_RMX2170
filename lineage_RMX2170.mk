66599997b33b6c69e8f77fd1a84600f0572eee32
Souljacker1 <sameerpyo39@gmail.com>
2022-06-16 21:33:53 +0530

    adapt for rd

--- lineage_RMX2170.mk
+++ lineage_RMX2170.mk
@@ -8,13 +8,13 @@
 $(call inherit-product, device/realme/RMX2170/device.mk)
 
 # Inherit some common EvolutionX stuff
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
-PRODUCT_NAME := evolution_RMX2170
+PRODUCT_NAME := lineage_RMX2170
 PRODUCT_DEVICE := RMX2170
 PRODUCT_BRAND := Realme
 PRODUCT_MODEL := Realme 7 Pro
@@ -32,13 +32,15 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="RMX2170"
 
 # Input additional flags here.
+RICE_MAINTAINER := SAMMY
 WITH_GAPPS := true
 TARGET_GAPPS_ARCH := arm64
-#TARGET_PIXEL_CHARGE_ANIM := true
+TARGET_HAS_UDFPS := true
+TARGET_ENABLE_BLUR := true
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_SUPPORTS_BLUR := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
-EXTRA_UDFPS_ANIMATIONS := true
-TARGET_SUPPORTS_NOW_PLAYING := true
-USE_PIXEL_CHARGER_IMAGES := true
-TARGET_BUILD_GRAPHENEOS_CAMERA := false
+#TARGET_PIXEL_CHARGE_ANIM := true
+#EXTRA_UDFPS_ANIMATIONS := true
+#TARGET_SUPPORTS_NOW_PLAYING := true
+#USE_PIXEL_CHARGER_IMAGES := true
+#TARGET_BUILD_GRAPHENEOS_CAMERA := false
